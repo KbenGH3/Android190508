1414aef7c9546cde332fcc71cca6c60eb56cc7b5
KbenGH3 <GH1b2e3n>
2019-05-09 22:25:23 +0800

    Layout Setting

--- app/src/main/java/com/example/testandroid2019/MainActivity.java
+++ app/src/main/java/com/example/testandroid2019/MainActivity.java
@@ -5,6 +5,9 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.text.method.MovementMethod;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -19,14 +22,14 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        FloatingActionButton fab = findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
     }
 
     @Override
@@ -85,11 +88,15 @@ public class MainActivity extends AppCompatActivity {
         TextView txvLog;
         txvLog=findViewById(R.id.txtLog);
 
+        TextView txvLo2;
+        txvLo2=findViewById(R.id.txtLOG2);
+
         //new date();
         StrLog="log";
 
-        txvLog.setText(txvLog.getText()+ "\r\n" + StrLog);
-
+//        txvLog.setText(txvLog.getText()+ "\r\n" + StrLog);
+        txvLo2.setText(txvLo2.getText()+ "\n" + StrLog);
+//        txvLo2.setMovementMethod(ScrollingMovementMethod.getInstance());
     }
     public void Test(){
 
